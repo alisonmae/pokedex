3c528e86a67f06c965d850fe45e0c4c33b49dd0e
Alison Bonham <alison@alisonmae.com>
2016-02-26 15:28:43 -0700

    giphy changes

--- giphy.rb
+++ giphy.rb
@@ -1,20 +1,16 @@
 require 'httparty'
 require 'pry'
-# require_relative 'pksearch.rb'
+require_relative 'pksearch.rb'
+require_relative 'cli.rb'
 
-
-
-puts "Welcome to Pokedex!"
-
-puts "What Pokemon would you like to choose?"
 
 # Open Pokemon gif based on keyword
-url = "http://api.giphy.com/v1/gifs/search?q=#{name}&api_key=dc6zaTOxFJmzC"
+url = "http://api.giphy.com/v1/gifs/search?q=#{@name}&api_key=dc6zaTOxFJmzC"
 response = HTTParty.get(url)
 pokemon_array = response["data"]
 random_pokemon_hash = pokemon_array.sample
-# .sample takes a random element / you could also do (array.length or shuffle.first)
 pokemon_url = random_pokemon_hash["url"]
+binding.pry
 system("open #{pokemon_url}")
 
 puts "Goodye!"
