795c7e0c6e89f334916e0cbfd9ad279a5f22b9de
Alison Bonham <alison@alisonmae.com>
2016-02-26 16:24:01 -0700

    cli giphy embed

--- cli.rb
+++ cli.rb
@@ -1,5 +1,7 @@
 require_relative 'pksearch'
 
+
+
 puts "Welcome to your Pokédex!"
 puts "What Pokémon do you want to look up? You can search by name or Pokédex ID."
 name_or_id = gets.chomp.downcase
@@ -9,3 +11,15 @@ pk = PKSearch.new (name_or_id)
 puts "\n~[Pokédex Search]~"
 puts "Pokemon name: #{pk.name}"
 puts "Pokedex #: #{pk.id}"
+
+
+
+url = "http://api.giphy.com/v1/gifs/search?q=#{pk.name}&api_key=dc6zaTOxFJmzC"
+response = HTTParty.get(url)
+pokemon_array = response["data"]
+random_pokemon_hash = pokemon_array.sample
+pokemon_url = random_pokemon_hash["url"]
+binding.pry
+system("open #{pokemon_url}")
+
+puts "Goodye!"
