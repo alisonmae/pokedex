1bd1da8357cf3f714d2f12f4a53f955c4715cb12
Lauren Zugai <lauren@zugai.com>
2016-02-26 15:26:22 -0600

    fixed CLI, working on types

--- cli.rb
+++ cli.rb
@@ -4,4 +4,8 @@ puts "Welcome to your Pokédex!"
 puts "What Pokémon do you want to look up? You can search by name or Pokédex ID."
 name_or_id = gets.chomp.downcase
 
-PKSearch.new (name_or_id)
+pk = PKSearch.new (name_or_id)
+
+puts "\n~[Pokédex Search]~"
+puts "Pokemon name: #{pk.name}"
+puts "Pokedex #: #{pk.id}"
--- pksearch.rb
+++ pksearch.rb
@@ -2,16 +2,23 @@ require 'httparty'
 require 'pry'
 
 class PKSearch
-  attr_accessor :name_or_id, :name, :id
+  attr_accessor :name_or_id, :name, :id, :type, :type2
 
   def initialize(name_or_id)
     @name_or_id = name_or_id
     @name = get_name(name_or_id)
     @id = get_id(name_or_id)
 
-    puts "\n~[Pokédex Search]~"
-    puts "Pokemon name: #{name}"
-    puts "Pokedex #: #{id}"
+    # type = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][1]["type"]["name"]
+    # type2 = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][0]["type"]["name"]
+    #
+    # binding.pry
+    #
+    # if type2 != nil
+    #   puts "Type: #{type}  |  Type-2: #{type2}"
+    # else
+    #   puts "Type: #{type}"
+    # end
   end
 
   def get_name(name_or_id)
@@ -31,6 +38,6 @@ class PKSearch
   end
 end
 
-  # type = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][1]["type"]["name"]
-  # type2 = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][0]["type"]["name"]
-    # puts "Types: #{types[1]} #{types[2]}"
+  # def get_types(name_or_id)
+  #   type = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][1]["type"]["name"]
+  #   type2 = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{name_or_id}")["types"][0]["type"]["name"]
